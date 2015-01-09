8f07510f0cc0051f2565394704a24e1290c14850
jjaspe <jjaspe09@hotmail.com>
2015-01-08 23:52:27 -0500

    Added Reserve,Remove, ManageWishList methods and views

--- WishList/Controllers/UserController.cs
+++ WishList/Controllers/UserController.cs
@@ -21,7 +21,10 @@ namespace WishList.Controllers
             String username = db.getLogInUserName();
             User user = db.People.SingleOrDefault(c => c.userName.Equals(username));
             if (user != null)
+            {
+                ViewBag.User = user;
                 return View(db.People.ToList().Where(c => c.Id != user.Id));
+            }
             else
                 return RedirectToAction("LoggedOut");
         }
@@ -42,14 +45,6 @@ namespace WishList.Controllers
                 return HttpNotFound();
             }
             return View(user);
-        }
-
-        //
-        // GET: /User/Create
-
-        public ActionResult Create()
-        {
-            return View();
         }
 
 
@@ -82,8 +77,89 @@ namespace WishList.Controllers
             return View(user);
         }
 
-        
+        public ActionResult ManageWishList(int watchedUserId = 0)
+        {
+            String username = db.getLogInUserName();
+            User loggedUser = db.People.SingleOrDefault(c => c.userName.Equals(username)),
+                watchedUser = db.People.SingleOrDefault(c => c.Id == watchedUserId);
+            ViewBag.LoggedUser = loggedUser;
+            ViewBag.WatchedUser = watchedUser;
+            if (watchedUser != null)
+            {
+                //if (user.WishListItems == null)
+                //    user.WishListItems = new List<WishListItem>();
+                return View(watchedUser.WishListItems);
+            }
 
+            return null;
+        }
+
+        [HttpPost]
+        public ActionResult ManageWishList(string[] reservedItems, string[] removedItems, string action, int? watchedUserId)
+        {
+            if (action.Equals("Reserve Selected"))
+                return Reserve(reservedItems, watchedUserId);
+            else if (action.Equals("Remove Selected"))
+                return Remove(removedItems);
+            else
+                return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// Reserves to loggedUserId items requested by watchedUserId.
+        /// </summary>
+        /// <param name="reservedItems">list of ids of reserved items</param>
+        /// <param name="watchedUserId"></param>
+        /// <returns></returns>
+        public ActionResult Reserve(string[] reservedItems, int? watchedUserId)
+        {
+            WishListItem current;
+            int itemId;
+            String username = db.getLogInUserName();
+            User loggedUser = db.People.SingleOrDefault(c => c.userName==username),
+                watchedUser = db.People.SingleOrDefault(c => c.Id == watchedUserId);
+
+
+            if (reservedItems != null && loggedUser != null && watchedUser != null && watchedUser.Id != loggedUser.Id)
+            {
+                foreach (string idString in reservedItems)
+                {
+                    itemId = Int32.Parse(idString);
+                    current = db.Gifts.SingleOrDefault(c => c.Id == itemId);
+                    if (current != null)
+                    {
+                        current.giver = loggedUser;
+                        db.Entry(current).State = EntityState.Modified;
+                    }
+                }
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// Removes items from loggedUser's wish list
+        /// </summary>
+        /// <param name="removedItems">array of ids of items to remove</param>
+        /// <param name="watchedUserId"></param>
+        /// <returns></returns>
+        public ActionResult Remove(string[] removedItems)
+        {
+            WishListItem current;
+            int id;
+            if (removedItems != null)
+            {
+                foreach (string idString in removedItems)
+                {
+                    id = Int32.Parse(idString);
+                    current = db.Gifts.SingleOrDefault(c => c.Id == id);
+                    if (current != null)
+                        db.Gifts.Remove(current);
+                }
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
